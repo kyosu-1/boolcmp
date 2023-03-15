bd3d6d7df944cac7e826a3cfce0a96ae2468c4d5
kyosu-1 <sho013039@gmail.com>
2023-03-16 08:39:55 +0900

    テンプレート

---
+++ judgebool_test.go
@@ -0,0 +1,15 @@
+package judgebool_test
+
+import (
+	"testing"
+
+	"github.com/gostaticanalysis/testutil"
+	"github.com/kyosu-1/judgebool"
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+// TestAnalyzer is a test for Analyzer.
+func TestAnalyzer(t *testing.T) {
+	testdata := testutil.WithModules(t, analysistest.TestData(), nil)
+	analysistest.Run(t, testdata, judgebool.Analyzer, "a")
+}
